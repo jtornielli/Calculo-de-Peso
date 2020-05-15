2ca494fe50b77c18cbe2b3e523d85f1d4bf1f0d2
jtornielli <64178000+jtornielli@users.noreply.github.com>
2020-05-15 08:50:46 -0300

    Funccionalidad terminado
    -funcciones finales
    -cambio de letra en resultado

--- js.js
+++ js.js
@@ -1,8 +1,9 @@
-let pesoAbonado;
-let pesoReal;
-let paisSeleccionado;
-let descuento = 0;
-let precioDelKilo = 0;
+let pesoAbonado; //txt Cuanto pago el cliente
+let pesoReal; // txt Cuanto pesa el producto
+let paisSeleccionado; // slc != Pais !='s precios
+let precioDelKilo = 0; // Valor del kg despues de descuentos comparado con pesoReal
+let precioAbonadoFinal = 0; // valor del kg despues de descuentos comparado con pesoAbonado
+let calculoFinal; // Cuenta final que mostramos en el pResultado
 $("#btnCalcular").click(calcularPeso);
 
 function calcularPeso(){
@@ -19,6 +20,15 @@ function calcularPeso(){
             if (pesoReal >= 5.1 ){
                 precioDelKilo = 9.95; // 50% de descuento = U$S 9.95 x kilo
             }
+            if(pesoAbonado >= 0.1 && pesoReal <= 3){
+                precioAbonadoFinal = 19.90; // U$S 19.90 x kilo 
+            }
+            if(pesoAbonado >= 3.1 && pesoReal <= 5){
+                precioAbonadoFinal = 13.93; //30% de descuento = U$S 13.93 x kilo
+            }
+            if (pesoAbonado >= 5.1 ){
+                precioAbonadoFinal = 9.95; // 50% de descuento = U$S 9.95 x kilo
+            }
         }
         if(paisSeleccionado === "ar"){
             if(pesoReal >= 0.1 && pesoReal <= 3){
@@ -29,6 +39,15 @@ function calcularPeso(){
             }
             if (pesoReal >= 5.1 ){
                 precioDelKilo = 11.50; // 50% de descuento = U$S 11.50 x kilo
+            }
+            if(pesoAbonado >= 0.1 && pesoReal <= 3){
+                precioAbonadoFinal = 22.99; // U$S 22.99 x kilo 
+            }
+            if(pesoAbonado >= 3.1 && pesoReal <= 5){
+                precioAbonadoFinal = 16.09; //30% de descuento = U$S 16.09 x kilo
+            }
+            if (pesoAbonado >= 5.1 ){
+                precioAbonadoFinal = 11.50; // 50% de descuento = U$S 11.50 x kilo
             }
         }
         if(paisSeleccionado === "pe"){
@@ -41,6 +60,15 @@ function calcularPeso(){
             if (pesoReal >= 5.1 ){
                 precioDelKilo = 9.90; // 50% de descuento = U$S 9.90 x kilo
             }
+            if(pesoAbonado >= 0.1 && pesoReal <= 3){
+                precioAbonadoFinal = 19,81 ; // U$S 19,81 x kilo 
+            }
+            if(pesoAbonado >= 3.1 && pesoReal <= 5){
+                precioAbonadoFinal = 13.69; //30% de descuento = U$S 13.69 x kilo
+            }
+            if (pesoAbonado >= 5.1 ){
+                precioAbonadoFinal = 9.90; // 50% de descuento = U$S 9.90 x kilo
+            }
         }
         if(paisSeleccionado === "br"){
             if(pesoReal >= 0.1 && pesoReal <= 3){
@@ -52,7 +80,19 @@ function calcularPeso(){
             if (pesoReal >= 5.1 ){
                 precioDelKilo = 9.90; // 50% de descuento = U$S 9.90 x kilo
             }
+            if(pesoAbonado >= 0.1 && pesoReal <= 3){
+                precioAbonadoFinal = 23.40 ; // U$S 23.40 x kilo 
+            }
+            if(pesoAbonado >= 3.1 && pesoReal <= 5){
+                precioAbonadoFinal = 13.69; //30% de descuento = U$S 13.69 x kilo
+            }
+            if (pesoAbonado >= 5.1 ){
+                precioAbonadoFinal = 9.90; // 50% de descuento = U$S 9.90 x kilo
+            }
         }
-        
-
+    calculoFinal = Math.round((pesoReal * precioDelKilo) - (pesoAbonado * precioAbonadoFinal));
+    $("#pResultado").html("Tiene que pagar : " + calculoFinal + " USD");
+            if(paisSeleccionado === "x"){
+                $("#pResultado").html("Tenes que seleccionar un pais!");
+            }
 }
